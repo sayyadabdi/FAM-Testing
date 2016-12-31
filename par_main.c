a301c3e8ba7c0daf01b1330528d21ac97ce0174d
Amir Hossein Sayyad Abdi <amirahsa@SPACER-PC.AHSA>
2016-12-31 16:26:26 +0330

    Tryingg223

--- par_main.c
+++ par_main.c
@@ -91,10 +91,10 @@ int main()
         while(array[PROBLEM_SIZE] != 0); // wait for others
         // Array is partialy sorted, so we have to merge them.
         mergeSortedParts(array, myEndPosition);
-                for(i = 0; i <= PROBLEM_SIZE; i++)
+                for(i = 65500; i <= PROBLEM_SIZE; i++)
                 {
             printf("%d\n", array[i]);
-            if(i%5 == 0)
+            if(i%1000 == 0)
             getchar();
             }
             }
